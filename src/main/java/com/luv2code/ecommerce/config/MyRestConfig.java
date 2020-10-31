0150918b32e9e03bb245217de442807b9aa52afb
esrahankonyar <konyar.esrahan@gmail.com>
2020-10-31 15:49:39 +0300

    Initial commit

---
+++ src/main/java/com/luv2code/ecommerce/config/MyRestConfig.java
@@ -0,0 +1,29 @@
+package com.luv2code.ecommerce.config;
+
+import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyRestConfig  implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        //Product tablosu için POST, PUT, DELETE metodlarını kullanıma engelleyeceğiz. Yani sadece Get metodu ile veri okuma işlemi yapılabilecek.
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //ProductCategory tablosu için POST, PUT, DELETE metodlarını kullanıma engelleyeceğiz. Yani sadece Get metodu ile veri okuma işlemi yapılabilecek.
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
